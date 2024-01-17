40a32f205950c797a2d00fe17f6b59164eee8055
Nandhinidevi@123 <nandhini.devi@thirvusoft.in>
2024-01-17 13:11:09 +0000

    add prospect button in lead

--- core_js/public/js/lead.js
+++ core_js/public/js/lead.js
@@ -1,10 +1,28 @@
 frappe.ui.form.on('Lead', {
     refresh(frm) {
+        frm.add_custom_button(
+            __("Prospect"),
+            function () {
+                frappe.call({
+                    method: "core_js.core_js.utils.leads.create_prospect",
+                    args: {
+                        "doc": frm.doc.name
+                    },
+                    callback: function(r){
+                        if (r.message){
+                           frappe.set_route('Form', 'Prospect', r.message);
+                        }
+                    }
+                })
+            },
+            __("Create")
+        );
         if(cur_frm.doc.custom_make_read_only){
             setTimeout(()=>{
             frm.remove_custom_button("Customer","Create");
             frm.remove_custom_button("Opportunity","Create");
             frm.remove_custom_button("Quotation","Create");
+            frm.remove_custom_button("Prospcet","Create");
             frm.remove_custom_button("Call");
             frm.remove_custom_button("Next Follow-up");
         },100)
